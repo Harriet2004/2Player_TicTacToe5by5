84c9c47439c2302e9553ac508701b008dd74f2cc
Harriet2004 <s3933558@student.rmit.edu.au>
2024-11-16 02:25:49 +1100

    Just formatted and removed some things from normal 3*3 JS file

--- 3*3player.js
+++ 3*3player.js
@@ -51,13 +51,14 @@ function changePlayer() {
 function winner() {
     let wins = false;
     let winningCombo = null;
+    
     for (let i = 0; i < winConditions.length; ++i) {
         const condition = winConditions[i];
         const cellA = options[condition[0]];
         const cellB = options[condition[1]];
         const cellC = options[condition[2]];
 
-        if(cellA == "" || cellB == "" || cellC == "") {
+        if (cellA == "" || cellB == "" || cellC == "") {
             continue
         } 
         if (cellA == cellB && cellB == cellC) {
